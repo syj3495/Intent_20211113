f2564473e018ef925572aa0acb6464542084ee58
yeonjin seong <syj3405@naver.com>
2021-11-14 01:16:03 +0900

    복귀 기능 - Intent 연습문제   - 화면 이동시 이전 화면이 계속 누적됨.

--- app/src/main/java/com/example/intent_20211113/OtherActivity.kt
+++ app/src/main/java/com/example/intent_20211113/OtherActivity.kt
@@ -1,14 +1,24 @@
 package com.example.intent_20211113
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
 
+        btnReturnToMain.setOnClickListener {
 
+            val returnIntent = Intent(this,MainActivity::class.java)
+
+            startActivity(returnIntent)
+
+
+
+        }
 
     }
 }
