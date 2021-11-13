95026d174e7ed6e58e3fafb817c09c932ed1fb30
yeonjin seong <syj3405@naver.com>
2021-11-14 01:05:30 +0900

    다른 화면 추가하기   - xml UI 수정

---
+++ app/src/main/java/com/example/intent_20211113/OtherActivity.kt
@@ -0,0 +1,14 @@
+package com.example.intent_20211113
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+
+
+    }
+}
